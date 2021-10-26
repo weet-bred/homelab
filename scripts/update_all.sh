fb745e063b6cb63cd4a5ba681a65550d380a571d
Peter Engels <pengels@omen.pengels.local>
2021-10-26 16:12:45 -0500

    updated the update script to update virus definitions and such also removed hostnames since I'm putting it on github publicly

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -3,23 +3,42 @@
 ## Declare our variables
 dnf_systems=$(cat /home/pengels/configs/dnf_systems.txt)
 apt_systems=$(cat /home/pengels/configs/apt_systems.txt)
+suricata_systems=$(cat /home/pengels/configs/suricata_systems.txt)
+openvas_systems=$(cat /home/pengels/configs/openvas_systems.txt)
+pihole_systems=$(cat /home/pengels/configs/pihole_systems.txt)
 
 for system in ${dnf_systems}
 do
 	echo ${system}
-	ssh -o ConnectTimeout=8 ${system} "sudo dnf update -y"
+	ssh -o ConnectTimeout=10 ${system} "sudo dnf update -y"
 done
 
 for system in ${apt_systems}
 do
 	echo ${system}
-	ssh -o ConnectTimeout=8 ${system} "sudo apt update -y"
-	ssh -o ConnectTimeout=8 ${system} "sudo apt updgrade -y"
+	ssh -o ConnectTimeout=10 ${system} "sudo apt update -y"
+	ssh -o ConnectTimeout=10 ${system} "sudo apt updgrade -y"
 done
 
 # Special Software
+for system in ${pihole_systems}
+do
+	echo ${system}
+	ssh -o ConnectTimeout=10 ${system} "sudo pihole -up"
+done
 
-echo "raspberrypi.pengels.local"
-ssh -o ConnectTimeout=8 raspberrypi.pengels.local "sudo pihole -up"
-echo "fujitsu.pengels.local"
-ssh -o ConnectTimeout=8 fujitsu.pengels.local "sudo suricata-update"
+for system in ${suricata_systems}
+do
+	echo ${system}
+	ssh -o ConnectTimeout=10 ${system} "sudo suricata-update"
+done
+
+for system in ${openvas_systems}
+do
+	echo ${system}
+	ssh -o ConnectTimeout=10 ${system} "sudo greenbone-certdata-sync"
+	ssh -o ConnectTimeout=10 ${system} "sudo greenbone-feed-sync"
+	ssh -o ConnectTimeout=10 ${system} "sudo greenbone-nvt-sync"
+	ssh -o ConnectTimeout=10 ${system} "sudo greenbone-scapdata-sync"
+done
+
