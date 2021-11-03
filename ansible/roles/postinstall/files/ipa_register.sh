df9a5eaf0ba1eaf9b5a1be5a2e66749250dc2e5a
Peter Engels <pengels@omen.pengels.local>
2021-11-03 11:53:07 -0500

    added notification for manual steps and removed the ipa_register.sh from the playbook since it needs interactive action

--- ansible/roles/postinstall/files/ipa_register.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-admin_user=$(cat /home/pengels/configs/admin_user.txt)
-admin_pw=$(cat /home/pengels/configs/admin_pw.txt)
-freeipa_server=$(cat /home/pengels/configs/freeipa_servers.txt)
-# Set the hostname to something normal
-fqdn_lower_hostname=$(hostname | sed 's/pengels.local//' | tr '[:upper:]' '[:lower:]').pengels.local 
-# Register the client with all the options and 
-ipa-client-install --domain=pengels.local --server=${freeipa_server} --mkhomedir --no-dns-sshfp --fixed-primary --hostname=${fqdn_lower_hostname} --principal ${admin_user} --password=${admin_pw} --no-ntp --force-join
