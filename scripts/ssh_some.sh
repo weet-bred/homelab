69d861c351e615d9b26edc599ffbd9b6799062b9
Peter Engels <pengels@omen.pengels.local>
2021-11-08 22:49:29 -0600

    added a script that will allow me to ssh to just some of the systems, not all

---
+++ scripts/ssh_some.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script simply allows a one-command ssh to all the systems in case I want to edit a config file or something on all machines
+# Last Edit: adding documentation
+# Date of last edit: see git log
+
+## Declare our variables
+# Systems need to be quoted with a space between hostnames
+systems=$1
+
+# SSH to the systems one at a time
+for system in ${systems}
+do
+	echo ${system}
+	ssh -o ConnectTimeout=10 ${system} "${2}"
+done
